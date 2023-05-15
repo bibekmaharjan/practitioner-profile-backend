10924474cf47bd4e7cc1916cabeb8d71419f4835
Bibek Maharjan <bibekmaharjan12@gmail.com>
2023-05-15 15:34:01 +0545

    Add route to handle add practitioner request

--- src/controller/practitioner.ts
+++ src/controller/practitioner.ts
@@ -1,4 +1,5 @@
 import db from '../models';
+import cloudinary from '../services/cloudinary';
 
 const Practitioner = db.practitioner;
 
@@ -13,6 +14,41 @@ export const getPractitioner = async (req, res) => {
     const practitioners = await Practitioner.findAll();
 
     res.send(practitioners);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+/**
+ * Add practitioner to practitioner list.
+ *
+ * @param {Request} req
+ * @param {Response} res
+ */
+export const addPractitioner = async (req, res) => {
+  try {
+    const result = await cloudinary.uploader.upload(req.file.path);
+    const imageUrl = result.secure_url;
+
+    const createdPractitioner = await Practitioner.create({
+      fullName: req.body.fullName,
+      email: req.body.email,
+      contact: req.body.contact,
+      dob: req.body.dob,
+      workingDays: req.body.workingDays,
+      startTime: req.body.startTime,
+      endTime: req.body.endTime,
+      address: req.body.address,
+      city: req.body.city,
+      gender: req.body.gender,
+      zipcode: req.body.zipcode,
+      status: req.body.status,
+      isICUSpecialist: req.body.isICUSpecialist,
+      userImg: imageUrl,
+      allergies: req.body.allergies,
+    });
+
+    res.send(createdPractitioner);
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
--- src/routes/practitioner.routes.ts
+++ src/routes/practitioner.routes.ts
@@ -1,4 +1,10 @@
-import { getPractitioner } from '../controller/practitioner';
+import multer from 'multer';
+
+import practitioner from '../schemas/practitioner';
+import { schema } from '../middleware/validateSchema';
+import { addPractitioner, getPractitioner } from '../controller/practitioner';
+
+const upload = multer({ dest: 'uploads/' });
 
 export default (app) => {
   app.use(function (req, res, next) {
@@ -7,4 +13,6 @@ export default (app) => {
   });
 
   app.get('/practitioners', getPractitioner);
+
+  app.post('/practitioners', [upload.single('userImg'), schema(practitioner)], addPractitioner);
 };
