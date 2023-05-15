9fd35cf7118ca6dcddfabfcfbaedd535773dec69
Bibek Maharjan <bibekmaharjan12@gmail.com>
2023-05-15 14:32:57 +0545

    Backend initialization

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+const port = 8080;
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
