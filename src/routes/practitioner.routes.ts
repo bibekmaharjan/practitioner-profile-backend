8596dca17179e70875c6e4ad477fb782afe0f94a
Bibek Maharjan <bibekmaharjan12@gmail.com>
2023-05-17 06:20:07 +0545

    Add middleware for cors

--- src/routes/practitioner.routes.ts
+++ src/routes/practitioner.routes.ts
@@ -1,10 +1,8 @@
 import { getPractitioner } from '../controller/practitioner';
+import corsMiddleware from '../middleware/corsMiddleware';
 
 export default (app) => {
-  app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Headers', 'x-access-token, Origin, Content-Type, Accept');
-    next();
-  });
+  app.use(corsMiddleware);
 
   app.get('/practitioners', getPractitioner);
 };
